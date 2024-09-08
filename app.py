f2cea232c55e951c6e4d25fc3fe9be770074250c
Laya Dang <pd03@bu.edu>
2024-09-08 18:43:36 -0400

    testing books display for frontend

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 # terminal run $ flask --app app --debug run
 from flask import Flask, render_template
+import csv
+import os
+import pandas as pd
+
+# path to book information (will change this to a database call)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, 'data/cleaned_book_info.csv')
+
+df = pd.read_csv(file_path)
+titles = df['title'].tolist()
+covers = df['cover'].tolist()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    var = "Daniel"
-    return render_template("index.html", name=var)
+    return render_template("index.html",
+                           zip=zip,
+                           titles=titles, 
+                           covers=covers)
